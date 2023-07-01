9f6f07b4b6b5279430c05fda2722e3e3ab46c69c
abdulghapur <gofurbrente01@gmail.com>
2023-07-01 21:07:44 +0700

    Update: game list footer

--- lib/game_list.dart
+++ lib/game_list.dart
@@ -17,12 +17,13 @@ class GameList extends StatelessWidget {
         color: Colors.white,
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: Container(
+        child: Stack(children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Container(
                   padding: const EdgeInsets.all(16.0),
                   decoration: const BoxDecoration(
                     borderRadius: BorderRadius.only(
@@ -31,31 +32,76 @@ class GameList extends StatelessWidget {
                     color: Color(0xFFE51C8B),
                   ),
                   child: AutoSizeText(
-                  "Permainan Pilihan".toUpperCase(),
-                  style: const TextStyle(
+                    "Permainan Pilihan".toUpperCase(),
+                    style: const TextStyle(
+                        fontFamily: "Monserrat",
+                        color: Colors.white,
+                        fontSize: 24.0,
+                        decoration: TextDecoration.none),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(left: 16.0),
+                child: AutoSizeText(
+                  "Belajar Mengenali Bencana",
+                  style: TextStyle(
                       fontFamily: "Monserrat",
-                      color: Colors.white,
+                      color: Color(0xFF2D74B6),
                       fontSize: 24.0,
                       decoration: TextDecoration.none),
                   textAlign: TextAlign.center,
                 ),
               ),
-            ),
-
-            const Padding(
-              padding: EdgeInsets.only(left: 16.0),
-              child: AutoSizeText(
-                "Belajar Mengenali Bencana",
-                style: TextStyle(
-                    fontFamily: "Monserrat",
-                    color: Color(0xFF2D74B6),
-                    fontSize: 24.0,
-                    decoration: TextDecoration.none),
-                textAlign: TextAlign.center,
+              const SizedBox(
+                height: 8.0,
               ),
-            ),
-          ],
-        ),
+              const Padding(
+                padding: EdgeInsets.only(left: 16.0),
+                child: AutoSizeText(
+                  "Pelajari bencana melalui peta-peta yang menarik",
+                  style: TextStyle(
+                      fontFamily: "Monserrat",
+                      color: Colors.black,
+                      fontSize: 16.0,
+                      decoration: TextDecoration.none),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+              bottom: 0.0,
+              child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 40.0,
+                  decoration:  BoxDecoration(
+                    borderRadius: const BorderRadius.only(
+                        topRight: Radius.circular(50),
+                        topLeft: Radius.circular(50)),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: const Center(
+                    child: AutoSizeText(
+                      "© 2023 Yayasan Plan International Indonesia",
+                      style: TextStyle(
+                          fontFamily: "Monserrat",
+                          color: Colors.black,
+                          fontSize: 16.0,
+                          decoration: TextDecoration.none),
+                      textAlign: TextAlign.center,
+                    ),
+                  )))
+        ]),
       ),
     )));
   }

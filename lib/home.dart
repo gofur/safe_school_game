6b112e9e4f7289e66ee5404823680025f2328f50
abdulghapur <gofurbrente01@gmail.com>
2023-07-01 10:31:53 +0700

    .gitignore is now working

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,45 +9,78 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScaffoldMessenger(
-      child: PreferredSize(
-        preferredSize: const Size.fromHeight(150.0),
+      child: SafeArea(
         child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
           Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
+            color: Colors.white,
+            padding: const EdgeInsets.only(top: 80, bottom: 0.0),
+            child: const AspectRatio(
+              aspectRatio: 6 / 1,
+              child: Image(
+                image: AssetImage(
+                  'assets/images/logo.png',
+                ),
+                width: 10,
+                height: 10,
+              ),
+            ),
+          ),
+          Stack(
+            children: [
+              Container(
                 color: Colors.white,
-                border: Border.all(color: Colors.lightBlue, width: 0.0)),
-            height: 180,
+                child: Center(
+                  child: CustomPaint(
+                    painter: CurvePainter(),
+                    child: const Center(
+                        child: Padding(
+                      padding: EdgeInsets.only(top: 140.0),
+                    )),
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 24, right: 24, top: 120),
+                child: AutoSizeText(
+                  "Selamat datang di permainan sekolah aman".toUpperCase(),
+                  style: const TextStyle(
+                      fontFamily: "Monserrat",
+                      color: Colors.white,
+                      fontSize: 36.0,
+                      decoration: TextDecoration.none),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
           ),
           Expanded(
             child: Container(
-              color: Colors.white,
-              child: Container(
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.white, width: 0.0),
-                    color: const Color(0xFFE51C8B),
-                    borderRadius: const BorderRadius.vertical(
-                      top: Radius.elliptical(200, 160),
-                    ),
-                  ),
-                  child: Column(children: [
-                    Container(
-                        padding: const EdgeInsets.only(
-                            right: 16.0, left: 16.0, top: 100.0),
-                        child: const Text(
-                          "Selamat datang di permainan sekolah aman",
-                          style: TextStyle(
-                            fontFamily: "Monserrat",
-                            color: Colors.white,
-                          ),
-                          textAlign: TextAlign.center,
-                        ))
-                  ])),
+              color: const Color(0xFFE51C8B),
+              child: const SizedBox(
+                width: double.infinity,
+              ),
             ),
           ),
         ]),
       ),
     );
   }
+}
+
+class CurvePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()..color = const Color(0xFFE51C8B);
+    // create a path
+    Path path = Path();
+    path.moveTo(0, size.height * 50 * 50);
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(
+        size.width / 2, size.height - 180, size.width, size.height);
+    path.lineTo(size.width, size.height);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }

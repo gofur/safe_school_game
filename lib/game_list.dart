a8a48b8f350bbf8e92df22ec3b29587b360cb7f8
abdulghapur <gofurbrente01@gmail.com>
2023-07-01 15:53:24 +0700

    Update: add game list

--- lib/game_list.dart
+++ lib/game_list.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:safe_school_game/utils/custom_appbar.dart';
 
@@ -12,7 +13,50 @@ class GameList extends StatelessWidget {
       appBar: const CustomAppBar(
         preferredHeight: 100,
       ),
-      body: Container(color: Colors.white, child: const Text("Game List")),
+      body: Container(
+        color: Colors.white,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Container(
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(50),
+                        bottomRight: Radius.circular(50)),
+                    color: Color(0xFFE51C8B),
+                  ),
+                  child: AutoSizeText(
+                  "Permainan Pilihan".toUpperCase(),
+                  style: const TextStyle(
+                      fontFamily: "Monserrat",
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      decoration: TextDecoration.none),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+
+            const Padding(
+              padding: EdgeInsets.only(left: 16.0),
+              child: AutoSizeText(
+                "Belajar Mengenali Bencana",
+                style: TextStyle(
+                    fontFamily: "Monserrat",
+                    color: Color(0xFF2D74B6),
+                    fontSize: 24.0,
+                    decoration: TextDecoration.none),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
     )));
   }
 }

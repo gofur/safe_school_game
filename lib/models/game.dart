e102b8ee22c3d7b3569d462916ee922497cfb93b
abdulghapur <gofurbrente01@gmail.com>
2023-07-07 19:45:22 +0700

    Add: model game

---
+++ lib/models/game.dart
@@ -0,0 +1,14 @@
+
+class Game {
+  final String url;
+  final String image;
+  final String icon;
+  final String? titleIcon;
+  
+  Game({
+    required this.url,
+    required this.image,
+    required this.icon,
+    required this.titleIcon
+  });
+}
